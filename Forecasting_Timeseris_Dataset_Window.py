dde6cab160a16a453d844411fa799cfe2edee423
Mohamed Ashraf <58575039+amenamen00@users.noreply.github.com>
2023-06-19 12:34:13 +0300

    Create Forecasting_Timeseris_Dataset_Window.py
    Dataset Window Method for time series .

---
+++ Forecasting_Timeseris_Dataset_Window.py
@@ -0,0 +1,59 @@
+import tensorflow as tf
+
+#Creating Dataset Sample
+
+dataset = tf.data.Dataset.range(10)
+dataset = dataset.window(5, shift=1)
+for window_dataset in dataset:
+    for val in window_dataset:
+        print(val.numpy(), end=" ")
+    print()
+
+
+#Creating Dataset Window
+dataset = tf.data.Dataset.range(10)
+dataset = dataset.window(5, shift=1, drop_remainder=True)
+for window_dataset in dataset:
+    for val in window_dataset:
+        print(val.numpy(), end=" ")
+    print()
+
+
+#Patching Dataset 
+dataset = tf.data.Dataset.range(10)
+dataset = dataset.window(5, shift=1, drop_remainder=True)
+dataset = dataset.flat_map(lambda window: window.batch(5))
+for window in dataset:
+    print(window.numpy())
+
+
+#Samples into Features and Labels
+dataset = tf.data.Dataset.range(10)
+dataset = dataset.window(5, shift=1, drop_remainder=True)
+dataset = dataset.flat_map(lambda window: window.batch(5))
+dataset = dataset.map(lambda window: (window[:-1], window[-1:]))
+for x, y in dataset:
+    print(x.numpy(), y.numpy())
+
+
+#Shuffling and Patch Sizing
+dataset = tf.data.Dataset.range(10)
+dataset = dataset.window(5, shift=1, drop_remainder=True)
+dataset = dataset.flat_map(lambda window: window.batch(5))
+dataset = dataset.map(lambda window: (window[:-1], window[-1:]))
+dataset = dataset.shuffle(buffer_size=10)
+dataset = dataset.batch(2).prefetch(1)
+for x, y in dataset:
+    print("x =", x.numpy())
+    print("y =", y.numpy())
+    
+#Final Dataset window Method for time seris  
+def window_dataset(series, window_size, batch_size=32,
+                   shuffle_buffer=1000):
+    dataset = tf.data.Dataset.from_tensor_slices(series)
+    dataset = dataset.window(window_size + 1, shift=1, drop_remainder=True)
+    dataset = dataset.flat_map(lambda window: window.batch(window_size + 1))
+    dataset = dataset.shuffle(shuffle_buffer)
+    dataset = dataset.map(lambda window: (window[:-1], window[-1]))
+    dataset = dataset.batch(batch_size).prefetch(1)
+    return dataset
